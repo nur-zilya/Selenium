9ec7880e5f7992e4d243e70e0edaa6fb5dc26721
zilya <nur-zilya@mail.ru>
2024-06-18 20:43:55 +0300

    dropdown

---
+++ lesson 16/dropdown.py
@@ -0,0 +1,28 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+
+options = Options()
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--user-agent=Automation QA (Windows NT 6.1; rv:106.0) Gecko/20100101 Firefox/106.0")
+options.page_load_strategy = "normal"
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+wait = WebDriverWait(driver, 10, poll_frequency=1)
+
+driver.get("http://the-internet.herokuapp.com/dropdown")
+
+DROPDOWN_ELEMENT = ("xpath", "//select[@id='dropdown']")
+
+DROPDOWN = Select(driver.find_element(*DROPDOWN_ELEMENT))
+
+# DROPDOWN.select_by_visible_text("Option 2")
+
+DROPDOWN.select_by_value("2")
